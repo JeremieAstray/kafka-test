1eb6bc7b04e69df3f1781708e3a9603c05731c1c
叶冠鸿 <5490584810@163.com>
2016-06-04 21:00:50 +0800

    init project

---
+++ src/main/java/com/jeremie/kafka/Main.java
@@ -0,0 +1,10 @@
+package com.jeremie.kafka;
+
+/**
+ * @author guanhong 16/6/4 下午8:58.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/com/jeremie/kafka/MainTest.java
@@ -0,0 +1,10 @@
+package com.jeremie.kafka;
+
+/**
+ * @author guanhong 16/6/4 下午8:58.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
